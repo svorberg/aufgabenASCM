f78b67e6276d2638ffcde88b11501fa5f2cc13b6
svorberg <sebastian.vorberg@stud.hshl.de>
2019-03-13 16:50:58 +0100

    Push - Ende

--- src/aufgabeEins/Main.java
+++ src/aufgabeEins/Main.java
@@ -12,7 +12,84 @@ public class Main {
 
 		ArrayList<String> list1 = getFileAsArrayList("src/aufgabeEins/List1.txt");
 		ArrayList<String> list2 = getFileAsArrayList("src/aufgabeEins/List2.txt");
+		ArrayList<String> onlyIn1 = getComparedAMinusB(list1, list2);
+		ArrayList<String> onlyIn2 = getComparedAMinusB(list2, list1);
+		ArrayList<String> inBoth = getComparedAAndB(list1, list2);
+		jsonOutputForAscm(onlyIn1, onlyIn2, inBoth);
+	}
 
+	public static void jsonOutputForAscm(ArrayList<String> onlyIn1, ArrayList<String> onlyIn2,
+			ArrayList<String> inBoth) {
+
+		System.out.println("{");
+		System.out.println("\t" + "\"onlyInList1\":\t[");
+		boolean start = true;
+		for (String s : onlyIn1) {
+			if (start) {
+				System.out.print("\t\t\"" + s + "\"");
+				start = false;
+			} else {
+				System.out.print(",\n\t\t\"" + s + "\"");
+			}
+		}
+		System.out.println("\n\t],");
+		System.out.println("\t" + "\"onlyInList2\":\t[");
+		start = true;
+		for (String s : onlyIn2) {
+			if (start) {
+				System.out.print("\t\t\"" + s + "\"");
+				start = false;
+			} else {
+				System.out.print(",\n\t\t\"" + s + "\"");
+			}
+		}
+		System.out.println("\n\t],");
+		System.out.println("\t" + "\"inBothLists\":\t[");
+		start = true;
+		for (String s : inBoth) {
+			if (start) {
+				System.out.print("\t\t\"" + s + "\"");
+				start = false;
+			} else {
+				System.out.print(",\n\t\t\"" + s + "\"");
+			}
+		}
+		System.out.println("\n\t]");
+		System.out.println("}");
+
+	}
+
+	public static ArrayList<String> getComparedAMinusB(ArrayList<String> a, ArrayList<String> b) {
+		ArrayList<String> list = new ArrayList<>();
+		for (String s1 : a) {
+			boolean treffer = false;
+			for (String s2 : b) {
+				if (s1.equals(s2)) {
+					treffer = true;
+					break;
+				}
+			}
+			if (treffer == false) {
+				list.add(s1);
+			}
+
+		}
+		return list;
+	}
+
+	public static ArrayList<String> getComparedAAndB(ArrayList<String> a, ArrayList<String> b) {
+		ArrayList<String> list = new ArrayList<>();
+		for (String s1 : a) {
+			for (String s2 : b) {
+				if (s1.equals(s2)) {
+					list.add(s1);
+					break;
+
+				}
+			}
+
+		}
+		return list;
 	}
 
 	/**
